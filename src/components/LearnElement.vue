3d69a3ba419ba1eca67c2cdda23bef9fddd4ddeb
fenglixiong <fenglixiong123@163.com>
2019-05-13 00:39:08 +0800

    element demo

---
+++ src/components/LearnElement.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-progress type="circle" :percentage="0"></el-progress>
+    <el-progress type="circle" :percentage="25"></el-progress>
+    <el-progress type="circle" :percentage="100" status="success"></el-progress>
+    <el-progress type="circle" :percentage="50" status="exception"></el-progress>
+    <br/>
+    <div>
+      <el-row>
+        <el-col :span="6"><span style="float: left">1</span></el-col>
+        <el-col :span="6">2</el-col>
+        <el-col :span="6">3</el-col>
+        <el-col :span="6">4</el-col>
+      </el-row>
+    </div>
+    <div>
+      <el-button type="primary">点我</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LearnElement',
+  data () {
+    return {
+      msg: 'Welcome'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import LearnElement from '@/components/LearnElement'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode:'history',
+  linkActiveClass:'active',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/learnElement',
+      name: 'LearnElement',
+      component: LearnElement
     }
   ]
 })
