bc97af6a0d81da38c53072a706d1e2ba3f24371b
fenglixiong <fenglixiong123@163.com>
2019-05-17 00:56:43 +0800

    删除一些杂乱文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import LearnElement from '../components/LearnElement'
 import Home from "../components/Home";
 import Guide from "../components/Guide";
 import Setting from "../components/Setting";
-import activePublic from '../page/activePublic'
-import step1 from '../page/activePublic/step1'
-import step2 from '../page/activePublic/step2'
-import step3 from '../page/activePublic/step3'
-import step4 from '../page/activePublic/step4'
 import Statistic from "../components/Statistic";
 
 Vue.use(Router);
@@ -22,27 +16,13 @@ export default new Router({
       redirect:'/activeHome'
     },
     {
-      path: '/learnElement',
-      name: 'LearnElement',
-      component: LearnElement
-    },
-    {
       path:'/activeHome',
       name:'Home',
       component:Home
     },
     {
-      path:'/activePublic',
-      name:'activePublic',
-      component:activePublic,
-      children:[
-        //路径为'/activePublic'，使用组件step1
-        // {path:'',component:step1},
-        {path:'step1',component:step1},
-        {path:'step2',component:step2},
-        {path:'step3',component:step3},
-        {path:'step4',component:step4},
-      ]
+      path:'/login',
+      component:()=>import('../page/login')  //路由懒加载,提高效率
     },
     {
       path:'/activeStatistic',
@@ -58,6 +38,10 @@ export default new Router({
       path:'/systemSet',
       name:'Setting',
       component:Setting
+    },
+    {
+      path:'*',
+      redirect: '/404'
     }
   ]
 })
